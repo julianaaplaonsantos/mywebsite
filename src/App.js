787f542995d2771703b580f12abd9bac99e73e4b
Juliana Aplaon Santos <japlaonsantos@gmail.com>
2025-08-09 21:27:36 +0800

    Update multiple files with latest changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import Navbar from "./components/Navbar";
 import Home from "./sections/Home";
-import About from "./sections/About"; // This must match your About.tsx location
+import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 export default function App() {
