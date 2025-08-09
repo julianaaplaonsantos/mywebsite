3854570778a80b0bbabd56b80b5de85d62847064
Juliana Aplaon Santos <japlaonsantos@gmail.com>
2025-08-10 03:32:16 +0800

    Rebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Important! This tells Vite your app runs at root, not in a subfolder.
-  server: {
-    open: false,
-  },
-});
+  base: '/mywebsite/'
+})
